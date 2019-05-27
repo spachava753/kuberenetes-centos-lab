85d612c9b3a26ace71c3bc9daebc9887f28a072f
Shashank Pachava <spachava753@gmail.com>
2019-05-27 17:43:06 -0400

    Check in lab

---
+++ post_cluster_misc.sh
@@ -0,0 +1,5 @@
+scp vagrant@172.42.42.100:/home/vagrant/.kube/config .
+kubectl --kubeconfig=config get nodes
+kubectl --kubeconfig=config get po
+kubectl --kubeconfig=config get po -A
+kubectl --kubeconfig=config get po -A -o wide
